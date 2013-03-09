5a1e660b8a7a2f0e8c19a1033b3c894ebbb19474
Nic Roland <nicroland9@gmail.com>
2013-03-09 19:46:10 +0000

    Added some comments, removed display_context() as it's redundant.

--- pyack.py
+++ pyack.py
@@ -25,13 +25,16 @@ class Context(object):
             file_line = int(args[2])
 
             self.get_context(file_path, file_line)
-            self.display_context()
+            self.print_dict(self.context)
         elif len(args) == 2:
             self.grep_for(args[1])
         else:
             print self.USAGE
 
     def add_context(self, cntx_list):
+        """
+        Adds a node to the context tree.
+        """
         pointer = self.context[cntx_list[0]]
         for step in cntx_list[1:-1]:
             if step not in pointer:
@@ -43,7 +46,7 @@ class Context(object):
 
     def get_indent(self, line):
         """
-        Get the indentation on a line as a substring.
+        Returns the indentation on a line as a substring.
         """
         match = re.match(self.INDENT_RE, line)
         indent = match.group(1)
@@ -51,9 +54,12 @@ class Context(object):
         return re.sub(self.TAB, self.FOUR_SPACES, indent)
 
     def get_context(self, file_path, file_line):
+        """
+        Given the file path and the line number, determine the context of that line.
+        """
         file_indx = file_line - 1
 
-        # Add file to context
+        # Add file path node to context
         if not file_path in self.context:
             self.context[file_path] = {}
 
@@ -65,32 +71,33 @@ class Context(object):
                     lines.append(line)
                 else:
                     break
-
         assert len(lines) == file_line
+
+        # Get indent of specified line
         init_indent = self.get_indent(lines[file_indx])
 
-        results = []
+        results = [self.LINE_SUM_COLOUR % (file_line, lines[file_indx].strip('\n'))]
         for line_no in range(file_indx-1, -1, -1):
+            # Ignore empty lines
             if lines[line_no].strip():
+                # Ignore lines that aren't a function or a class
                 kw_match = re.search(self.DEF_CLASS_RE, lines[line_no])
                 if kw_match:
+                    # Get indent of relevant lines
                     next_indent = self.get_indent(lines[line_no])
 
+                    # if line has less indentation add to results, change init_indent
                     if len(next_indent) < len(init_indent):
                         results.append(
                             ' '.join(kw_match.groups())
                             )
                         init_indent = next_indent
 
+        # Each context entry should also contain file path
         results.append(file_path)
         results = list(reversed(results))
-        results.append(
-                self.LINE_SUM_COLOUR % (file_line, lines[file_indx].strip('\n'))
-                )
-        self.add_context(results)
 
-    def display_context(self):
-        self.print_dict(self.context)
+        self.add_context(results)
 
     def print_dict(self, obj, counter=0):
         for key, val in obj.iteritems():
@@ -124,7 +131,7 @@ class Context(object):
             file_name, file_line = row.split(':')[:2]
 
             self.get_context(file_name, int(file_line))
-        self.display_context()
+        self.print_dict(self.context)
 
 if __name__ == "__main__":
     x = Context(sys.argv)
