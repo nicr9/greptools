0654e1529d986d9f69d31b66c699294d6e7a37ce
Nic Roland <nicroland9@gmail.com>
2013-03-15 10:19:54 +0000

    Added a setup.py (and a .gitignore to hide build/)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='pygt',
+        version='0.1',
+        description='Python Grep Tool',
+        author='Nic Roland',
+        author_email='nicroland9@gmail.com',
+        scripts = ['pygt']
+        )
