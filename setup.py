dabac54877534a735685979bd2fbf6f5b1439bbe
Nic Roland <nicroland9@gmail.com>
2013-05-24 08:30:03 +0100

    Added a few things to .gitignore and bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='pygt',
-        version='0.1',
+        version='0.2a',
         description='Python Grep Tool',
         author='Nic Roland',
         author_email='nicroland9@gmail.com',
