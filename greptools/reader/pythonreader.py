b1cd2fd3df168bd34abba547714aeeb7129b9933
Nic Roland <nicroland9@gmail.com>
2014-10-25 09:56:54 +0100

    Refactored some logic out of PythonReader into IndentReader.

--- greptools/reader/pythonreader.py
+++ greptools/reader/pythonreader.py
@@ -1,15 +1,9 @@
 import re
 from .reader import BaseReader
 
-class PythonReader(BaseReader):
-    """An implementation of a Reader for Python code."""
-    # CONSTANTS
-    INDENT_RE = re.compile("^(\s*)")
-    DEF_CLASS_RE = re.compile("^\s*(def|class) (.*?)[(:]")
+class IndentReader(BaseReader):
     TAB = '\t'
     FOUR_SPACES = '    '
-    FILE_PATTERNS = ['*.py']
-    TYPE = 'Python'
 
     def _get_indent(self, line):
         """
@@ -46,11 +40,10 @@ class PythonReader(BaseReader):
                 # if line has less indentation 
                 if len(next_indent) < len(init_indent):
                     # Ignore lines that aren't a function or a class
-                    kw_match = re.search(self.DEF_CLASS_RE, lines[line_no])
-                    if kw_match:
+                    if self._line_match(lines[line_no]):
                         # add to results
                         results.append(
-                            ' '.join(kw_match.groups())
+                            self._parse_line(lines[line_no])
                             )
 
                     # then change init_indent
@@ -64,3 +57,21 @@ class PythonReader(BaseReader):
                 reversed(results)
                 )
 
+
+class PythonReader(IndentReader):
+    """An implementation of a Reader for Python code."""
+    # CONSTANTS
+    INDENT_RE = re.compile("^(\s*)")
+    DEF_CLASS_RE = re.compile("^\s*(def|class) (.*?)[(:]")
+    FILE_PATTERNS = ['*.py']
+    TYPE = 'Python'
+
+    def _line_match(self, line_text):
+        if re.search(self.DEF_CLASS_RE, line_text):
+            return True
+        else:
+            return False
+
+    def _parse_line(self, line_text):
+        match = re.search(self.DEF_CLASS_RE, line_text)
+        return ' '.join(match.groups())
