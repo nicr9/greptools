8295645f443e5a80784dadac6a708482a7c9a76c
Nic Roland <nicroland9@gmail.com>
2015-12-27 13:00:46 +0000

    Various improvements to setup.py and .gitignore.
    distutils -> setuptools, autogen README.rst, install_requires,
    test_suite and some style fixes.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+VERSION = '0.9a'
+
+try:
+    import subprocess
+    subprocess.call(["pandoc", "--from=markdown", "--to=rst", "README.md", "-o", "README.rst"])
+except:
+    pass
 
 setup(
         name='greptools',
-        version='v0.9a',
-        description='A collection of grep wrappers for various filetypes.',
+        version=VERSION,
+        description='A collection of grep wrappers and tools for various filetypes.',
         author='Nic Roland',
         author_email='nicroland9@gmail.com',
-        packages=['greptools', 'greptools.reader'],
-        scripts = ['bin/pygt', 'bin/mdgt', 'bin/javagt'],
-        long_description=open('README.md').read(),
+        url='https://github.com/nicr9/greptools',
+        download_url='https://github.com/nicr9/greptools/tarball/%s' % VERSION,
+        packages=['greptools', 'greptools.reader', "greptools.test"],
+        scripts=['bin/pygt', 'bin/mdgt', 'bin/javagt'],
+        install_requires=['mock'],
+        test_suite="greptools.test",
+        long_description=open('README.rst').read(),
         )
